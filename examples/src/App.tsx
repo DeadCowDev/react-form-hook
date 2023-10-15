3adf235d200097bd42828c326324e28f9e8c95a7
Diogo Viana <diogo.g.viana@gmail.com>
2023-10-15 18:39:07 +0100

    add example

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -1,3 +1,5 @@
+import { useForm } from "@deadcow-enterprises/react-form-hook";
+import { z } from "zod";
 import "./App.css";
 
 type LoginModel = {
@@ -6,7 +8,60 @@ type LoginModel = {
 };
 
 function App() {
-  return <form></form>;
+  const form = useForm<LoginModel>({
+    username: {
+      value: "",
+      validator: z.string().min(1, "Username is required"),
+    },
+    password: {
+      value: "",
+      validator: z.string().min(1, "Password is required"),
+    },
+  });
+  const login = (formValue: LoginModel) => {
+    // do something with formValue here like send it to the server
+    console.log(formValue);
+  };
+  return (
+    <form onSubmit={form.handleSubmit(login)}>
+      <label htmlFor="username">
+        Username
+        <input
+          type="text"
+          name="username"
+          id="username"
+          value={form.value.username}
+          onChange={(ev) => {
+            form.setValue("username", ev.target.value);
+          }}
+        />
+        {form.errors.username?.map((err, i) => (
+          <span className="error" key={i}>
+            {err}
+          </span>
+        ))}
+      </label>
+      <label htmlFor="password">
+        Password
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={form.value.password}
+          onChange={(ev) => {
+            form.setValue("password", ev.target.value);
+          }}
+        />
+        {form.errors.username?.map((err, i) => (
+          <span className="error" key={i}>
+            {err}
+          </span>
+        ))}
+      </label>
+
+      <button>Submit</button>
+    </form>
+  );
 }
 
 export default App;
