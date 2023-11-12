ffa25ca09d2dc22c6408ef2262a4e458e74924e0
Diogo Viana <diogo.g.viana@gmail.com>
2023-11-12 15:13:49 +0000

    allow promises and add loading flag

--- src/types.ts
+++ src/types.ts
@@ -14,11 +14,11 @@ export type FormOptions<T extends Record<string, any>> = {
 export interface FormResult<T> {
   value: T;
   dirty: boolean;
+  loading: boolean;
   errors: { [P in keyof T]?: FormError };
-
   markAsDirty: () => void;
   addError: (k: keyof T, error: string) => void;
   setValue: <K extends keyof T>(k: K, value: T[K]) => FormError;
   reset: (newValue?: T) => void;
-  handleSubmit: (cb: (v: T) => void) => (ev: FormEvent) => void;
+  handleSubmit: (cb: (v: T) => void | Promise<void>) => (ev: FormEvent) => void;
 }
